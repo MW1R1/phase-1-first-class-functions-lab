d9b4fee69e39653f6f556a6f0366c7a132b431ef
MW1R1 <nickmwirigi58@gmail.com>
2023-03-19 19:04:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (cars) {  
+return cars.slice(0, 2); 
+}
+const returnLastTwoDrivers = function (cars) {  
+    return cars.slice(2); 
+    }
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = function(num){
+    function innerFunction(number){
+        return num * number;
+    }
+    return innerFunction;
+    } 
+    const fareDoubler = createFareMultiplier(2)
+    const fareTripler = createFareMultiplier(3)
+    function selectDifferentDrivers(drivers, fun) {
+        return fun (drivers)
+    }
+
